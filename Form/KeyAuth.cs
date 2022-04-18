e1271c0f021382fefed83982b0afe3419a36f16d
William Nelson <83034852+wnelson03@users.noreply.github.com>
2022-04-18 10:31:25 -0400

    Improve rate limit handling

--- Form/KeyAuth.cs
+++ Form/KeyAuth.cs
@@ -687,9 +687,8 @@ namespace KeyAuth
                 switch (response.StatusCode)
                 {
                     case (HttpStatusCode)429: // client hit our rate limit
-                        error("You're connecting too fast to loader, slow down.");
-                        Environment.Exit(0);
-                        return "";
+                        Thread.Sleep(1000);
+                        return req(post_data);
                     default: // site won't resolve. you should use keyauth.uk domain since it's not blocked by any ISPs
                         error("Connection failure. Please try again, or contact us for help.");
                         Environment.Exit(0);
